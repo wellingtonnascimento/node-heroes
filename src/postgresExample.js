97f7037d53b8a3a317da8f9546f59ece96c5205f
Wellington N <wellingto_07@hotmail.com>
2020-05-11 14:29:52 -0300

    iniciando sequelize e criando estrutura e conexão

--- src/postgresExample.js
+++ src/postgresExample.js
@@ -2,13 +2,43 @@
 
 const Sequelize = require('sequelize');
 const driver = new Sequelize(
-    'herois',
+    'heroes',
     'wellington',
-    '123456',
-    {
+    '123456', {
         host: 'localhost',
         dialect: 'postgres',
         quoteIndentifiers: false,
-        operator
+        operatorsAliases: false
     }
 );
+
+async function main(){
+    const Herois = driver.define('herois',{
+        id: {
+            type: Sequelize.INTEGER,
+            required: true,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome:{
+            type: Sequelize.STRING,
+            required: true
+        },
+        poder:{
+            type: Sequelize.STRING, 
+            required: true
+        }
+    },{
+        tableName: 'tb_herois',
+        freezeTableName: false,
+        timestamps: false
+    })
+    await Herois.sync();
+    // await Herois.create({
+    //     nome: 'Lanterna Verde',
+    //     poder: 'Anel'
+    // })
+    const result = await Herois.findAll({raw: true});
+    console.log('result', result);
+}
+main();
