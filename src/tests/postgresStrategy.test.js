593c50e04ef360fdf7636f503faa1eec9e88cdfa
Wellington N <wellingto_07@hotmail.com>
2020-05-14 10:39:18 -0300

    Refactoring strategy Postgres multi-schemas

--- src/tests/postgresStrategy.test.js
+++ src/tests/postgresStrategy.test.js
@@ -1,16 +1,20 @@
 const assert = require('assert');
-const Postgres = require('../db/strategies/postgres');
+const Postgres = require('../db/strategies/postgres/postgres');
+const HeroiSchema = require('../db/strategies/postgres/schemas/heroiSchema');
 const Context = require('../db/strategies/base/contextStrategy');
 
-const context = new Context(new Postgres());
+
 
 const MOCK_HEROI_CADASTRAR = {nome: 'Viuva', poder: 'Luta'};
 const MOCK_HEROI_ATUALIZAR = {nome: 'Mulher Maravilha', poder: 'Força'};
 
+let context = {};
 describe('Postgres Strategy', function(){
     this.timeout(Infinity)
     this.beforeAll(async function (){
-        await context.connect();
+        const connection = await Postgres.connect();
+        const model = await Postgres.defineModel(connection, HeroiSchema);
+        context = new Context(new Postgres(connection, model));
         await context.delete();
         await context.create(MOCK_HEROI_ATUALIZAR);
     })
