73d67bab6b8cae64fdd95b324754a4c5209f6759
Wellington N <wellingto_07@hotmail.com>
2020-05-16 09:53:37 -0300

    frame hapi, test injecct req, routes dinamicas

---
+++ src/api.js
@@ -0,0 +1,34 @@
+// npm i hapi
+
+const Hapi = require('hapi');
+const Context = require('./db/strategies/base/contextStrategy');
+const MongoDb = require('./db/strategies/mongodb/mongodb');
+const HeroiSchema = require('./db/strategies/mongodb/schemas/heroisSchema');
+const HeroRoute = require('./routes/heroesRoute');
+
+
+const app = new Hapi.server({
+    port: 5000
+});
+
+function mapRoutes(instance, methods){
+
+    return methods.map(method=> instance[method]());
+
+}
+
+async function main() {
+    const connection = MongoDb.connect();
+    const context = new Context(new MongoDb(connection, HeroiSchema));
+
+    app.route([
+        ...mapRoutes(new HeroRoute(context), HeroRoute.methods())
+    ])
+
+    await app.start();
+    console.log('Servidor rodando na porta', app.info.port);
+
+    return app;
+}
+
+module.exports = main();
