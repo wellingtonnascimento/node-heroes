a58c9ff7b28c69cf6ec370c1a43133d642379afc
Wellington N <wellingto_07@hotmail.com>
2020-05-16 10:56:14 -0300

    listar heroes api, validando com teste

--- src/routes/heroesRoute.js
+++ src/routes/heroesRoute.js
@@ -11,7 +11,27 @@ class HeroRoutes extends BaseRoute {
             path: '/herois',
             method: 'GET',
             handler: (request, headers) =>{
-                return this.db.read();
+                try{
+                    const {
+                        skip,
+                        limit,
+                        nome} = request.query
+                    console.log('limit', limit);
+
+                    let query = {};
+                    if(nome){
+                        query.nome = nome;
+                    }
+                    if(isNaN(skip))
+                        throw Error('O Tipo do skip e incorreto');
+                    if(isNaN(limit))
+                        throw Error('O Tipo do limit e incorreto');
+                                   
+                    return this.db.read(query, parseInt(skip), parseInt(limit))
+                }catch(error){
+                    console.log('DEU RUIM', error)
+                    return "Erro interno no servidor"
+                }
             }
         }
     }
--- src/tests/apiHero.test.js
+++ src/tests/apiHero.test.js
@@ -1,18 +1,53 @@
 const assert = require('assert');
 const api = require('./../api');
 
-describe('Suite de teste da API Heroes', function (){
+describe.only('Suite de teste da API Heroes', function (){
     this.beforeAll(async () => {
         app = await api;
     })
     it('Listar /herois', async () => {
         const result = await app.inject({
             method: 'GET',
-            url: '/herois'
+            url: '/herois?skip=0&limit=10'
         })
         const dados = JSON.parse(result.payload);
         const statusCode = result.statusCode;
         assert.deepEqual(statusCode, 200);
         assert.ok(Array.isArray(dados));
+    })
+    it('listar /herois = deve retornar somente 10 resgistos', async () => {
+        const TAMANHO_LIMIT =3;
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMIT}`
+        })
+
+        const dados = JSON.parse(result.payload);
+        const statusCode = result.statusCode;
+        assert.deepEqual(statusCode, 200);
+        assert.ok(dados.length === TAMANHO_LIMIT);
+    })
+    it('listar /herois = deve dar erro caso passe string', async () => {
+        const TAMANHO_LIMIT ='AEE';
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMIT}`
+        })
+        
+        assert.deepEqual(result.payload, 'Erro interno no servidor');
+    })
+
+    it('listar /herois = deve filtrar um item', async () => {
+        const TAMANHO_LIMIT =1000;
+        const NAME = 'Homem Aranha-1589397805677'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMIT}&nome=${NAME}`
+        })
+
+        const dados = JSON.parse(result.payload);
+        const statusCode = result.statusCode;
+        assert.deepEqual(statusCode, 200);
+        assert.ok(dados[0].nome === NAME);
     })
 })
